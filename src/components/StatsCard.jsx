95ba31ccf4119578053de5c086a30debaa2d12b2
HaiderMonther <alardyhydr567890@gmail.com>
2024-10-06 19:39:18 +0300

    three commit

---
+++ src/components/StatsCard.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { PiStudentDuotone } from "react-icons/pi";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { RiFileList2Line, RiContractLine } from "react-icons/ri";
+import { BsFillPeopleFill } from "react-icons/bs";
+
+function StatsCard({ governorate, students, teachers, courses, presentStudents, ongoingCourses }) {
+  return (
+    <div className="max-w-lg mx-auto my-4 p-4 bg-white rounded-xl shadow-md space-y-4">
+      <h2 className="text-xl font-medium text-center text-gray-700 mb-4">{governorate} - إحصائيات المحافظة</h2>
+      <div className="grid grid-cols-3 gap-2 text-center">
+        
+        {/* عدد الطلبة */}
+        <div className="bg-[#ECF2F5] text-[#1B3E5D] p-3 rounded-lg">
+          <p className="text-3xl">
+            <PiStudentDuotone />
+          </p>
+          <h3 className="text-lg mt-2">عدد الطلبة</h3>
+          <p className="text-lg font-semibold">{students}</p>
+        </div>
+
+        {/* عدد الأساتذة */}
+        <div className="bg-[#ECF2F5] text-[#1B3E5D] p-4 rounded-lg">
+          <p className="text-4xl">
+            <FaChalkboardTeacher />
+          </p>
+          <h3 className="text-lg mt-2">عدد الأساتذة</h3>
+          <p className="text-xl font-semibold">{teachers}</p>
+        </div>
+
+        {/* عدد الدورات */}
+        <div className="bg-[#ECF2F5] text-[#1B3E5D] p-4 rounded-lg">
+          <p className="text-4xl">
+            <RiFileList2Line />
+          </p>
+          <h3 className="text-lg mt-2">عدد الدورات</h3>
+          <p className="text-xl font-semibold">{courses}</p>
+        </div>
+
+        {/* الطلبة الحاضرين */}
+        <div className="bg-[#ECF2F5] text-[#1B3E5D] p-4 rounded-lg">
+          <p className="text-4xl">
+            <BsFillPeopleFill />
+          </p>
+          <h3 className="text-lg mt-2">الطلبة الحاضرين</h3>
+          <p className="text-xl font-semibold">{presentStudents}</p>
+        </div>
+
+        {/* الدورات المقامة */}
+        <div className=" text-[#1B3E5D] p-4 rounded-lg bg-[#ECF2F5] col-span-2">
+          <p className="text-4xl">
+            <RiContractLine />
+          </p>
+          <h3 className="text-lg mt-2">الدورات المقامة</h3>
+          <p className="text-xl font-semibold">{ongoingCourses}</p>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default StatsCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,19 +7,17 @@ import {
   format,
   getDay,
   isEqual,
-  isSameDay,
   isSameMonth,
   isToday,
   parse,
   isValid,
-  parseISO,
   startOfToday,
 } from "date-fns";
 import { arSA } from "date-fns/locale";
 import { useState } from "react";
 import { PiStudentDuotone } from "react-icons/pi";
 import { RiContractLine } from "react-icons/ri";
-
+import StatsCard from "../components/StatsCard";
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
@@ -27,10 +25,13 @@ function classNames(...classes) {
 function Home() {
   let today = startOfToday();
   let [selectedDay, setSelectedDay] = useState(today);
+
+
   let [currentMonth, setCurrentMonth] = useState(
-    format(today, "MMM-yyyy", { locale: arSA })
+    format(today, "yyyy-MM", { locale: arSA })
   );
-  let firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date(), {
+
+  let firstDayCurrentMonth = parse(currentMonth, "yyyy-MM", new Date(), {
     locale: arSA,
   });
 
@@ -45,113 +46,144 @@ function Home() {
 
   function previousMonth() {
     let firstDayPrevMonth = add(firstDayCurrentMonth, { months: -1 });
-    setCurrentMonth(format(firstDayPrevMonth, "MMM-yyyy", { locale: arSA }));
+    setCurrentMonth(format(firstDayPrevMonth, "yyyy-MM", { locale: arSA }));
   }
+
   function nextMonth() {
     let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
-    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy", { locale: arSA }));
+    setCurrentMonth(format(firstDayNextMonth, "yyyy-MM", { locale: arSA }));
   }
 
   return (
-    <div className="pt-16">
-      <div
-        className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6"
-        dir="ltr"
-      >
-        <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
-          <div className="md:pr-14">
-            <div className="flex items-center">
-              <h2 className="flex-auto font-semibold text-gray-900">
-                {isValid(firstDayCurrentMonth)
-                  ? format(firstDayCurrentMonth, "MMMM yyyy", { locale: arSA })
-                  : "Invalid Date"}
-              </h2>
-              <button
-                type="button"
-                onClick={previousMonth}
-                className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">Previous month</span>
-                <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
-              <button
-                onClick={nextMonth}
-                type="button"
-                className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">Next month</span>
-                <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
-              <div>S</div>
-              <div>M</div>
-              <div>T</div>
-              <div>W</div>
-              <div>T</div>
-              <div>F</div>
-              <div>S</div>
-            </div>
-            <div className="grid grid-cols-7 mt-2 text-sm">
-              {days.map((day, dayIdx) => (
-                <div
-                  key={day.toString()}
-                  className={classNames(
-                    dayIdx === 0 && colStartClasses[getDay(day)],
-                    "py-1.5"
-                  )}
+    <>
+      <div className="pt-16">
+        <div
+          className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6"
+          dir="ltr"
+        >
+          <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
+            <div className="md:pr-14">
+              <div className="flex items-center">
+                <h2 className="flex-auto font-semibold text-gray-900">
+                  {isValid(firstDayCurrentMonth)
+                    ? format(firstDayCurrentMonth, "MMMM yyyy", {
+                        locale: arSA,
+                      })
+                    : "Invalid Date"}
+                </h2>
+                <button
+                  type="button"
+                  onClick={previousMonth}
+                  className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
                 >
-                  <button
-                    type="button"
-                    onClick={() => setSelectedDay(day)}
+                  <span className="sr-only">Previous month</span>
+                  <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+                <button
+                  onClick={nextMonth}
+                  type="button"
+                  className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Next month</span>
+                  <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
+                <div>الأحد</div>
+                <div>الاثنين</div>
+                <div>الثلاثاء</div>
+                <div>الأربعاء</div>
+                <div>الخميس</div>
+                <div>الجمعة</div>
+                <div>السبت</div>
+              </div>
+              <div className="grid grid-cols-7 mt-2 text-sm">
+                {days.map((day, dayIdx) => (
+                  <div
+                    key={day.toString()}
                     className={classNames(
-                      isEqual(day, selectedDay) && "text-white",
-                      !isEqual(day, selectedDay) &&
-                        isToday(day) &&
-                        "text-red-500",
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        isSameMonth(day, firstDayCurrentMonth) &&
-                        "text-gray-900",
-                      !isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        !isSameMonth(day, firstDayCurrentMonth) &&
-                        "text-gray-400",
-                      isEqual(day, selectedDay) && isToday(day) && "bg-red-500",
-                      isEqual(day, selectedDay) &&
-                        !isToday(day) &&
-                        "bg-gray-900",
-                      !isEqual(day, selectedDay) && "hover:bg-gray-200",
-                      (isEqual(day, selectedDay) || isToday(day)) &&
-                        "font-semibold",
-                      "mx-auto flex h-8 w-8 items-center justify-center rounded-full"
+                      dayIdx === 0 && colStartClasses[getDay(day)],
+                      "py-1.5"
                     )}
                   >
-                    <time dateTime={format(day, "yyyy-MM-dd")}>
-                      {format(day, "d")}
-                    </time>
-                  </button>
-                </div>
-              ))}
+                    <button
+                      type="button"
+                      onClick={() => setSelectedDay(day)}
+                      className={classNames(
+                        isEqual(day, selectedDay) && "text-white",
+                        !isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          "bg-[#1B3E5D]",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-gray-900",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          !isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-gray-400",
+                        isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          "bg-[#1B3E5D]",
+                        isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          "bg-gray-900",
+                        !isEqual(day, selectedDay) && "hover:bg-gray-500",
+                        (isEqual(day, selectedDay) || isToday(day)) &&
+                          "font-semibold",
+                        "mx-auto flex h-8 w-8 items-center justify-center rounded-full"
+                      )}
+                    >
+                      <time dateTime={format(day, "yyyy-MM-dd")}>
+                        {format(day, "d")}
+                      </time>
+                    </button>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-          <div className="flex items-center justify-around" dir="rtl">
-            <div className="bg-white p-3 w-36 text-lg items-center flex flex-col justify-center">
-              <p className=" justify-center flex text-lg">
-                <PiStudentDuotone />
-              </p>
-              <h2 className=" text-center text-lg">الطلبة الحاضرين لهذا اليوم</h2>
-            </div>
-            <div className="bg-white p-3 w-36 text-lg items-center flex flex-col  justify-center">
-              <p className=" justify-center flex ">
-                <RiContractLine />
-              </p>
-              <h2 className=" text-center text-lg">الدورات المقامة اليوم</h2>
+            <div className="flex items-center justify-around" dir="rtl">
+              <div className=" bg-[#ffffff] rounded p-3 w-36 text-lg items-center flex flex-col justify-center text-[#1B3E5D] font-light">
+                <p className="text-4xl">
+                  <PiStudentDuotone />
+                </p>
+                <h2 className=" text-center text-base mt-1 ">
+                  الطلبة الحاضرين لهذا اليوم
+                </h2>
+                <p>12</p>
+              </div>
+              <div className=" bg-[#ffffff] p-3 rounded w-36 text-lg items-center flex flex-col  justify-center text-[#1B3E5D]">
+                <p className="text-4xl">
+                  <RiContractLine />
+                </p>
+                <h2 className=" text-center text-base mt-1 ">
+                  الدورات المقامة اليوم
+                </h2>
+                <p>12</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
+        <div>
+        <StatsCard
+          governorate="بغداد"
+          students={5000}
+          teachers={300}
+          courses={50}
+          presentStudents={4500}
+          ongoingCourses={5}
+        />
+        <StatsCard
+          governorate="النجف"
+          students={5000}
+          teachers={300}
+          courses={50}
+          presentStudents={4500}
+          ongoingCourses={5}
+        />
     </div>
+    </>
   );
 }
 
